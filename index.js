2a54e458ed772b5a30eb310dc8a4b088c4420cd9
Rajat Verma <rajatverma5885045@gmail.com>
2021-05-15 19:42:22 +0530

    add embed for snippets

---
+++ index.js
@@ -0,0 +1,24 @@
+// logic to get data and add cards
+fetch('./data.json')
+  .then((res) => res.json())
+  .then((res) => {
+    const content = document.getElementById('content');
+    let data = '';
+
+    res.forEach((el) => {
+      data += `
+        <div class="flex flex-col items-center">
+          <h1 class="text-purple-700 pt-4 pb-2 font-medium text-2xl">${el.title}</h1>
+          <iframe
+            src=${el.iframeURL}
+            height=${el.height}
+            width=680
+            style="max-width: 100%; border:0; transform: scale(1); overflow:hidden;"
+            sandbox="allow-scripts allow-same-origin">
+        </iframe>
+        </div>
+      `;
+    });
+
+    content.innerHTML = data;
+  });
